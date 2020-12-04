3fadc5f74b333017f3966b40cdf90fcef96079c7
Mathildasyy <119020045@link.cuhk.edu.cn>
2020-12-04 15:58:43 +0800

    Move&Delete

--- preprocessing/x000_DataPreprocess.r
+++ preprocessing/x000_DataPreprocess.r
@@ -16,35 +16,36 @@ summary(train)
 ggplot(train, aes(x = SalePrice/1000)) + # Measure the house prices in $1,000
   geom_histogram(binwidth = 30)
 
+train$SalePrice.log <- log(train$SalePrice/1000+1) # log transformation
+ggplot(train, aes(x = SalePrice.log)) + 
+  geom_histogram(binwidth = .1)
+
 #  We roughly abstract the class of each feature
 feature.class <- c()
-for (i in 2:80){
+for (i in 1:79){
   feature.class <- append(feature.class,class(train[,i]))
 }
 
 ## To observe qualitative features, we use boxplots to idnetify outliers.
 ## To observe quantitative features, we draw basis-spline plots.
-bsplot <- function(index, title){
-  data <- data.frame(x = train[,index],
-                     y = train$SalePrice/1000)
-  ggplot(data, aes(x,y)) + 
-    ggtitle("Cubic Polynomial Fit")+
-    theme(plot.title = element_text(hjust = 0.5))+
-    geom_point(col='grey60', size = 0.5) + 
-    geom_smooth(method="lm", formula= y~poly(x,3, raw =T), colour = 'hotpink3')
-}
 ## Set 2 by 2 canvas
 par(mfrow = c(2,2), mar = c(2,2,2,2))
 
 # Then, we plot the numeric and non-numeric variables in different ways
 feature.graphs <- function(subset.index){
 for (i in subset.index){
-  class <- class(train[i])
+  class <- feature.class[i]
   title <- names(train)[i]
   if(class == 'character'){
-    boxplot(SalePrice/1000~., data = train[,c(i,80)], main = title)
+    boxplot(SalePrice.log~., data = train[,c(i,81)], main = title)
     }else
-  bsplot(i, title)}
+  data <- data.frame(x = train[,i],
+                         y = train$SalePrice.log)
+  ggplot(data, aes(x,y)) + 
+    ggtitle(title)+
+    theme(plot.title = element_text(hjust = 0.5))+
+    geom_point(col='grey60', size = 0.5) + 
+    geom_smooth(method="lm", formula= y~poly(x,3, raw =T), colour = 'hotpink3')}
 }
 
 
@@ -62,7 +63,7 @@ feature.class[1:20] = sy
 ##feature.class[41:60] = hsq
 ##feature.class[61:79] = zqf
 
-# change the index to the subset of your part (e.g. sy: 1:20)
+# change the index to the subset
 feature.subset <- c(5,6,8,11,12,14,9,36,39,41,52,60,61,68,69,70,
                     71,74,22,43,45,46,55,27,30,31,35,72,75)
 
